77d65138fafa1f9cda6f243d65ed1783b2f381cc
GraveofBears <modf999@yahoo.com>
2022-11-09 15:28:59 -0500

    Add project files.

---
+++ GoodestBoy.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using BepInEx;
+using ItemManager;
+using CreatureManager;
+using HarmonyLib;
+using ServerSync;
+
+namespace GoodestBoy
+{
+	[BepInPlugin(ModGUID, ModName, ModVersion)]
+	public class GoodestBoy : BaseUnityPlugin
+	{
+		private const string ModName = "GoodestBoy";
+		private const string ModVersion = "0.1.4";
+		private const string ModGUID = "org.bepinex.plugins.goodestboy";
+
+		public void Awake()
+		{
+
+			Creature EvilBunny = new("gsd", "EvilBunny", "assets")              //add creature
+			{
+				Biome = Heightmap.Biome.Meadows,
+				CanBeTamed = false,
+				FoodItems = "Carrot",
+				SpawnChance = 30,
+				GroupSize = new Range(1, 2),
+				CheckSpawnInterval = 600,
+				SpecificSpawnTime = SpawnTime.Day,
+				RequiredWeather = Weather.ClearSkies,
+				Maximum = 1
+			};
+			EvilBunny.Drops["LeatherScraps"].Amount = new Range(1, 2);
+			EvilBunny.Drops["LeatherScraps"].DropChance = 10f;
+			EvilBunny.Drops["Carrot"].Amount = new Range(1, 2);
+			EvilBunny.Drops["Carrot"].DropChance = 10f;
+
+
+			Creature BrownEvilBunny = new("gsd", "BrownEvilBunny", "assets")              //add creature
+			{
+				Biome = Heightmap.Biome.Meadows,
+				CanBeTamed = false,
+				FoodItems = "Carrot",
+				SpawnChance = 30,
+				GroupSize = new Range(1, 2),
+				CheckSpawnInterval = 600,
+				SpecificSpawnTime = SpawnTime.Day,
+				RequiredWeather = Weather.ClearSkies,
+				Maximum = 1
+			};
+			BrownEvilBunny.Drops["LeatherScraps"].Amount = new Range(1, 2);
+			BrownEvilBunny.Drops["LeatherScraps"].DropChance = 10f;
+			BrownEvilBunny.Drops["Carrot"].Amount = new Range(1, 2);
+			BrownEvilBunny.Drops["Carrot"].DropChance = 10f;
+
+
+			Creature BestestDog = new("gsd", "BestestDog", "assets")            //add creature
+			{
+				Biome = Heightmap.Biome.BlackForest,
+				CanBeTamed = true,
+				FoodItems = "BestestStick, YummyBone, RawMeat, CookedMeat, BoarJerky, NeckTail, DeerStew, NeckTailGrilled, DeerMeat, CookedDeerMeat, MinceMeatSauce, Sausages, Entrails, SerpentMeatCooked, SerpentStew, SerpentMeat, CookedWolfMeat, WolfMeat, Wolfjerky, LoxMeat, CookedLoxMeat, LoxPie",
+				SpawnChance = 10,
+				GroupSize = new Range(1, 2),
+				CheckSpawnInterval = 300,
+				RequiredWeather = Weather.Rain,
+				SpecificSpawnTime = SpawnTime.Night,
+				Maximum = 1
+			};
+			BestestDog.Drops["BoneFragments"].Amount = new Range(1, 2);
+			BestestDog.Drops["BoneFragments"].DropChance = 15f;
+			BestestDog.Drops["WolfMeat"].Amount = new Range(1, 2);
+			BestestDog.Drops["WolfMeat"].DropChance = 50f;
+
+			Item BestestStick = new("gsd", "BestestStick", "assets");           //add item
+			BestestStick.Crafting.Add(CraftingTable.Workbench, 1);
+			BestestStick.RequiredItems.Add("Wood", 2);
+			BestestStick.CraftAmount = 1;
+
+			Item YummyBone = new("gsd", "YummyBone", "assets");                  //add item
+			YummyBone.Crafting.Add(CraftingTable.Workbench, 1);
+			YummyBone.RequiredItems.Add("BoneFragments", 4);
+			YummyBone.CraftAmount = 1;
+
+		}
+	}
+}
---
+++ ReadMe.txt
@@ -0,0 +1,43 @@
+
+<p align="center">
+<img src="https://i.imgur.com/g80lR3O.png">
+</p>
+
+<p align="center">
+A custom creature friend by OdinPlus Team.
+</p>
+
+<h2>  About: </h2>
+
+>Rabbits have invaded the meadows, they are eating all the berries and just being complete pests. What can we do? Well, never fear, GoodestBoy is here.
+>Find your "BestestDog" roaming in the forests, lost, scared, and looking for treats....
+
+>What treats might you ask?
+
+YummyBones & BestestStick!
+
+>That is right, the BestestDog loves these things and will tame pretty quick once finding them and be your forever friend.
+>When your BestestDog is happy he will even carry his own BestestStick!
+
+<p align="center">
+<img src="https://media.giphy.com/media/mrlehxQ3O3OWL6nEmM/giphy-downsized-large.gif">
+</p>
+
+<h2>  Install Notes: </h2>
+
+>Mod is required on both server and client for config sync to work with crafting requirements of YummyBone and BestestStick.
+
+<h2> Manual Install: </h2>
+
+>>Please download the latest copy of Bepinex per author's instructions.
+
+>>Place the GoodestBoy Folder inside of the "Bepinex\plugins\" folder.
+
+<p>
+
+<p align="center"><h2>For Questions or Comments find Gravebear in the Odin Plus Team on Discord:</h2></p>
+
+<p align="center"><a href="https://discord.gg/mbkPcvu9ax"><img src="https://i.imgur.com/Ji3u63C.png"></a></p>
+
+
+# This mod is using: <a href="https://github.com/blaxxun-boop/CreatureManager">CreatureManager & ItemManager</a> by Blaxxun.
