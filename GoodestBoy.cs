c498afb8b96964d01ef8715005ccab2d4bd327f6
GraveofBears <modf999@yahoo.com>
2023-12-09 16:57:12 -0500

    all the stuff

--- GoodestBoy.cs
+++ GoodestBoy.cs
@@ -23,7 +23,7 @@ namespace GoodestBoy
     public class GoodestBoy : BaseUnityPlugin
     {
         private const string ModName = "GoodestBoy";
-        private const string ModVersion = "0.1.11";
+        private const string ModVersion = "0.1.16";
         private const string ModGUID = "odinplus.plugins.goodestboy";
 
 
@@ -32,6 +32,9 @@ namespace GoodestBoy
 
         private static ConfigEntry<Toggle> serverConfigLocked = null!;
 
+        public static ConfigEntry<int> _goodestHealAmount = null!;
+        public static ConfigEntry<int> _goodestHealCooldown = null!;
+        public static ConfigEntry<int> _goodestDurabilityCost = null!;
 
         private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
         {
@@ -56,35 +59,35 @@ namespace GoodestBoy
         public void Awake()
         {
 
+
             GameObject sfx_dog_bark = ItemManager.PrefabManager.RegisterPrefab("gsd", "sfx_dog_bark"); //register projectile
             GameObject sfx_dog_panting = ItemManager.PrefabManager.RegisterPrefab("gsd", "sfx_dog_panting"); //register projectile
             GameObject sfx_gb_whistle = ItemManager.PrefabManager.RegisterPrefab("gsd", "sfx_gb_whistle");
 
             Item BestestTreat = new("gsd", "BestestTreat");
-            BestestTreat.Name.English("Bestest Treat");
-            BestestTreat.Description.English("A treat for your best friend, doesn't it look delicious.");
+            BestestTreat.Name.English("BestestTreat");
+            BestestTreat.Description.English("A treat for your best friend, doesn't it look delicious. This treat can be hand fed to hungry puppers.");
             BestestTreat.Crafting.Add(CraftingTable.Workbench, 1);
             BestestTreat.RequiredItems.Add("CookedMeat", 2);
-            BestestTreat.CraftAmount = 5;
-            GameObject BestestTreat_Projectile = ItemManager.PrefabManager.RegisterPrefab("gsd", "BestestTreat_Projectile"); //register projectile
+            BestestTreat.CraftAmount = 1;
 
             Item BestestBall = new("gsd", "BestestBall");
             BestestBall.Name.English("Bestest Ball");
-            BestestBall.Description.English("The only ball worthy of your bestest friend.");
+            BestestBall.Description.English("A dog toy, sometimes he will fetch, sometimes he wont, and sometimes he wont give it back.");
             BestestBall.Crafting.Add(CraftingTable.Workbench, 1);
             BestestBall.RequiredItems.Add("LeatherScraps", 2);
             BestestBall.CraftAmount = 1;
             GameObject BestestBall_Projectile = ItemManager.PrefabManager.RegisterPrefab("gsd", "BestestBall_Projectile"); //register projectile
 
-            Item GoodestWhistle = new("gsd", "GoodestWhistle");
-            GoodestWhistle.Name.English("GoodestWhistle");
-            GoodestWhistle.Description.English("A whistle to call your best friend.");
-            GoodestWhistle.Crafting.Add(CraftingTable.Workbench, 1);
-            GoodestWhistle.RequiredItems.Add("FineWood", 1);
-            GoodestWhistle.CraftAmount = 1;
-            var shared = GoodestWhistle.Prefab.GetComponent<ItemDrop>().m_itemData.m_shared;
+            Item GB_Whistle = new("gsd", "GB_Whistle");
+            GB_Whistle.Name.English("GoodestWhistle");
+            GB_Whistle.Description.English("A whistle to call your best friend. Warning, this may call all your friends if you have several.");
+            GB_Whistle.Crafting.Add(CraftingTable.Workbench, 1);
+            GB_Whistle.RequiredItems.Add("FineWood", 1);
+            GB_Whistle.CraftAmount = 1;
+            var shared = GB_Whistle.Prefab.GetComponent<ItemDrop>().m_itemData.m_shared;
             var statusEffect = ScriptableObject.CreateInstance<Recall>(); //sets the status effect script into this variable. 
-            statusEffect.m_ttl = 1f; // duration of the status effect.
+            statusEffect.m_ttl = .25f; // duration of the status effect.
             shared.m_consumeStatusEffect = statusEffect; // the item = the status effect.
 
 
@@ -94,10 +97,10 @@ namespace GoodestBoy
                 CanBeTamed = true,
                 FoodItems = "RawMeat, BestestTreat, CookedMeat, BoarJerky, NeckTail, DeerStew, NeckTailGrilled, DeerMeat, CookedDeerMeat, MinceMeatSauce, Sausages, Entrails, SerpentMeatCooked, SerpentStew, SerpentMeat, CookedWolfMeat, WolfMeat, Wolfjerky, LoxMeat, CookedLoxMeat, LoxPie,",
                 SpawnChance = 10,
-                RequiredWeather = Weather.None,
+                RequiredWeather = Weather.BlackForestFog,
                 GroupSize = new Range(1, 2),
                 CheckSpawnInterval = 300,
-                SpecificSpawnTime = SpawnTime.Night,
+                SpecificSpawnTime = SpawnTime.Always,
                 Maximum = 1
             };
             BestestDog.Drops["BoneFragments"].Amount = new Range(1, 2);
@@ -117,6 +120,9 @@ namespace GoodestBoy
             BestestPup.Drops["BoneFragments"].Amount = new Range(1, 2);
             BestestPup.Drops["BoneFragments"].DropChance = 50f;
 
+            _goodestHealAmount = Config.Bind("BestestTreat", "Amount", 100, "How much health the treat heals.");
+            _goodestHealCooldown = Config.Bind("BestestTreat", "Cooldown", 60, "How long the cooldown is in seconds.");
+            _goodestDurabilityCost = Config.Bind("BestestTreat", "Durability", 1, "How much durability the treat loses when used.");
 
             Assembly assembly = Assembly.GetExecutingAssembly();
 
---
+++ Patches/BestestTreat.cs
@@ -0,0 +1,91 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace GoodestBoy.Patches;
+
+public class BestestTreat
+{
+    internal static AssetBundle GetAssetBundle(string filename)
+    {
+        Assembly execAssembly = Assembly.GetExecutingAssembly();
+        string resourceName = execAssembly.GetManifestResourceNames().Single(str => str.EndsWith(filename));
+        using Stream stream = execAssembly.GetManifestResourceStream(resourceName);
+        return AssetBundle.LoadFromStream(stream);
+    }
+
+    [HarmonyPatch(typeof(Tameable), nameof(Tameable.GetHoverText))]
+    static class Tameable_transform_Patch
+    {
+        static void Postfix(Tameable __instance, ref string __result)
+        {
+            if (__instance.m_character.IsTamed())
+            {
+                var currentWeapon = Player.m_localPlayer.GetCurrentWeapon();
+                if (currentWeapon.m_dropPrefab?.name != "BestestTreat") return;
+                __result += $"\n[<color=yellow><b>L-Ctrl + E</b></color>] Heal: {GoodestBoy._goodestHealAmount.Value} HP";
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Tameable), nameof(Tameable.Interact))]
+    static class Tameable_Interact_Patch
+    {
+        static bool Prefix(Tameable __instance)
+        {
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                var lastTime = __instance.m_nview.m_zdo.GetInt("LastHealTime");
+                if (lastTime + GoodestBoy._goodestHealCooldown.Value > EnvMan.instance.m_totalSeconds)
+                {
+                    MessageHud.instance.ShowMessage(MessageHud.MessageType.Center, "Cooldown not ready");
+                    return false;
+                }
+
+                var currentWeapon = Player.m_localPlayer.GetCurrentWeapon();
+                if (currentWeapon.m_dropPrefab?.name != "BestestTreat") return true;
+
+                if (__instance.m_character.IsTamed())
+                {
+                    currentWeapon.m_durability -= GoodestBoy._goodestDurabilityCost.Value;
+                    if (currentWeapon.m_durability <= 0)
+                    {
+                        Player.m_localPlayer.UnequipItem(currentWeapon, false);
+                        Player.m_localPlayer.m_inventory.RemoveItem(currentWeapon);
+                    }
+
+                    Player.m_localPlayer.m_zanim.SetTrigger("interact");
+                    __instance.m_nview.InvokeRPC("GoodestHealTameable");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(Tameable), nameof(Tameable.Awake))]
+    static class Tameable_Awake_Patch
+    {
+        static void Postfix(Tameable __instance)
+        {
+            __instance.m_nview.Register("GoodestHealTameable", _ =>
+            {
+                var lastTime = __instance.m_nview.m_zdo.GetInt("LastHealTime");
+                if (lastTime + GoodestBoy._goodestHealCooldown.Value > EnvMan.instance.m_totalSeconds)
+                {
+                    return;
+                }
+
+                if (__instance.m_nview.IsOwner())
+                {
+                    __instance.m_nview.m_zdo.Set("LastHealTime", (int)EnvMan.instance.m_totalSeconds);
+                    __instance.m_character.Heal(GoodestBoy._goodestHealAmount.Value);
+                    Object.Instantiate(ZNetScene.instance.GetPrefab("fx_creature_tamed"), __instance.transform.position, Quaternion.identity);
+                }
+            });
+        }
+    }
+}
